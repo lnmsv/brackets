b60a2c8fc180bf2560c3733561c1c962870b3b0c
ln <lnmsv.io@gmail.com>
2020-10-25 16:25:21 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig, bracketsState = {}) {
+  let checkStr = (closeBracketToLookFor) => {
+    let currentChar = str[0];
+    str = str.slice(1);
+    if (closeBracketToLookFor !== undefined && currentChar === closeBracketToLookFor){
+      return true;
+    }
+    else{
+      let currentBrackets = bracketsConfig.find(x => x[0] === currentChar);
+      if (currentBrackets === undefined) return false;
+      let ret = (checkStr(currentBrackets[1]) && (closeBracketToLookFor === undefined || checkStr(closeBracketToLookFor)));
+      return ret;
+    }
+  };
+  let retValue = true;
+  while (str && str.length > 0 && retValue){
+    retValue = (retValue && checkStr());
+  }
+  return retValue;
 }
